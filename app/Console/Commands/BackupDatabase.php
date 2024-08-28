142b789832043444f54b859088af1d16117f0f3a
Basma-Abdallah <40538974+Basma-Abdallah@users.noreply.github.com>
2024-08-28 11:55:46 +0300

    Update BackupDatabase.php

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -27,11 +27,12 @@ class BackupDatabase extends Command
      */
     public function handle()
     {
-        $databaseName = env('laravelR6');
-        $username = env('root');
-        $password = env('');
-        $host = env('127.0.0.1');
+         $databaseName = env('DB_DATABASE');
+        $username = env('DB_USERNAME');
+        $password = env('DB_PASSWORD');
+        $host = env('DB_HOST');
         $port = env('DB_PORT', '3306');
+
 
         $date = now()->format('Y-m-d_H-i-s');
         $backupFile = "backup/{$databaseName}_{$date}.sql";
